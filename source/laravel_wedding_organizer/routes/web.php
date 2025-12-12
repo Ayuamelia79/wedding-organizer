91c541e2b2d3ec1db98226e6db878584d8970774
ayu amelia <ayuamelia7960@gmail.com>
2025-12-12 18:56:45 +0700

    update kedua

--- source/laravel_wedding_organizer/routes/web.php
+++ source/laravel_wedding_organizer/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengantinAuthController;
+use App\Http\Controllers\PaketController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,6 @@ Route::get('/', function () {
 Route::get('/login-pengantin', [PengantinAuthController::class, 'showLogin'])->name('login.pengantin.form');
 Route::post('/login-pengantin', [PengantinAuthController::class, 'login'])->name('login.pengantin');
 Route::get('/dashboard-pengantin', [PengantinAuthController::class, 'dashboard'])->middleware('auth');
+
+// Resource Paket
+Route::resource('paket', PaketController::class);
